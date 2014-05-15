5bcffbfc1438eeaa9ddfa07d0bb9693b245d9648
Kaustubh <Kaustubh.chavan@xoriant.com>
2014-05-15 17:37:39 +0530

    Changes to take input via Index.html for overlay ,height and position 15th May 2014

--- platforms/ios/DualCordovaWebviewPlugin/Plugins/DualCordovaWebviewPlugin.m
+++ platforms/ios/DualCordovaWebviewPlugin/Plugins/DualCordovaWebviewPlugin.m
@@ -14,14 +14,14 @@
 -(void)deviceready:(CDVInvokedUrlCommand*)command
 {
     self.webView.delegate = self;
-   
+    
     
     NSString* url = @"http://www.yahoo.com";
     NSURL* nsUrl = [NSURL URLWithString:url];
     NSURLRequest* request = [NSURLRequest requestWithURL:nsUrl cachePolicy:NSURLRequestReloadIgnoringLocalAndRemoteCacheData timeoutInterval:30];
     
     [self.webView loadRequest:request];
-
+    
 }
 
 
@@ -31,31 +31,32 @@
     
     NSMutableDictionary* properties = [command.arguments objectAtIndex:0];
     
-     NSString* position = [properties objectForKey:@"position"];
-    NSString* overlay = [properties objectForKey:@"overlay"];
+    NSString *position = [properties objectForKey:@"position"];
+    BOOL overlay = [[properties objectForKey:@"overlay"]boolValue];
+    NSString *url = [properties objectForKey:@"href"];
+    CGFloat webviewHeight = [[properties objectForKey:@"height"]floatValue];
     
-    if ([overlay isEqualToString:@"true"])
+    if (overlay == true)
     {
         if ([position isEqualToString:@"top"])
         {
             
-            [self.webView setFrame:CGRectMake(0, 120, 320, 460)];
-            UIWebView *webview = [[UIWebView alloc] initWithFrame:CGRectMake(0, 20, 320, 60)];
+            [self.webView setFrame:CGRectMake(0, webviewHeight, 320, 548-webviewHeight)];
+            UIWebView *webview = [[UIWebView alloc] initWithFrame:CGRectMake(0, 20, 320, webviewHeight)];
             
             [webview setBackgroundColor:[UIColor clearColor]];
-            NSString* url1 = @"http://www.google.com";
-            NSURL* nsUrl1 = [NSURL URLWithString:url1];
-            NSURLRequest* request1 = [NSURLRequest requestWithURL:nsUrl1 cachePolicy:NSURLRequestReloadIgnoringLocalAndRemoteCacheData timeoutInterval:30];
+            NSURL* nsUrl = [NSURL URLWithString:url];
+            NSURLRequest* request = [NSURLRequest requestWithURL:nsUrl cachePolicy:NSURLRequestReloadIgnoringLocalAndRemoteCacheData timeoutInterval:30];
             
-            [webview loadRequest:request1];
+            [webview loadRequest:request];
             [self.viewController.view addSubview:webview];
             
             
         }
         else{
             
-            [self.webView setFrame:CGRectMake(0, 20, 320, 460)];
-            UIWebView *webview = [[UIWebView alloc] initWithFrame:CGRectMake(0, 480, 320, 60)];
+            [self.webView setFrame:CGRectMake(0, 20, 320, 548 - webviewHeight)];
+            UIWebView *webview = [[UIWebView alloc] initWithFrame:CGRectMake(0, 548-webviewHeight, 320, webviewHeight)];
             
             [webview setBackgroundColor:[UIColor clearColor]];
             NSString* url1 = @"http://www.google.com";
@@ -68,12 +69,12 @@
             
         }
         
-
+        
     }
     else{
-    
-         [self.webView setFrame:CGRectMake(0, 0, 320, 520)];
-    
+        
+        [self.webView setFrame:CGRectMake(0, 0, 320, 548)];
+        
     }
     
     
